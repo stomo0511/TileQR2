2e522818fba5885ac08bb5bf9687a0a5996c2861
stomo <stomo@yamanashi.ac.jp>
2017-10-29 14:16:12 +0900

    [modify] delete local dynamic arraies in GEQRT and TSQRT

--- RightLooking.cpp
+++ RightLooking.cpp
@@ -25,6 +25,11 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 {
 	const int MT = A->mt();
 	const int NT = A->nt();
+	const int NB = A->mb(0,0);
+	const int IB = A->ib();
+
+	double *Work = new double [NB*IB];
+	double *Tau  = new double [NB];
 
 	double ttime = omp_get_wtime();
 
@@ -33,9 +38,6 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 	for (int tk=0; tk < min(MT,NT); tk++ )
 	{
 		{
-			double *Tau = new double [A->nb(tk,tk)];
-			double *Work = new double [A->nb(tk,tk)*A->ib()];
-
 			int info = core_dgeqrt( A->mb(tk,tk), A->nb(tk,tk), A->ib(),
 					A->ttop(tk,tk), A->mb(tk,tk),
 					T->ttop(tk,tk), T->mb(tk,tk),
@@ -50,23 +52,20 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 			#pragma omp critical
 			cout << omp_get_thread_num() << " : " << "GEQRT(" << tk << "," << tk << "," << tk << ") : " << omp_get_wtime() - ttime << "\n";
 			#endif
-
-			delete[] Tau;
-			delete[] Work;
 		}
 
 		#pragma omp parallel for
 		for (int tj=tk+1; tj < NT; tj++)
 		{
 			int nb = A->nb(tk,tj);
-			double *Work = new double [nb*A->ib()];
+			double *tWork = new double [nb*A->ib()];
 
 			int info = core_dormqr( PlasmaLeft, PlasmaTrans,
 					A->mb(tk,tj), A->nb(tk,tj), min(A->mb(tk,tk),A->nb(tk,tk)), A->ib(),
 					A->ttop(tk,tk), A->mb(tk,tk),
 					T->ttop(tk,tk), T->mb(tk,tk),
 					A->ttop(tk,tj), A->mb(tk,tj),
-					Work, nb);
+					tWork, nb);
 
 			if (info != PlasmaSuccess)
 			{
@@ -79,15 +78,12 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 			cout << omp_get_thread_num() << " : " << "LARFB(" << tk << "," << tj << "," << tk << ") : " << omp_get_wtime() - ttime << "\n";
 			#endif
 
-			delete[] Work;
+			delete[] tWork;
 		}
 
 		for (int ti=tk+1; ti < MT; ti++)
 		{
 			{
-				double *Tau = new double [A->nb(ti,tk)];
-				double *Work = new double [A->nb(ti,tk)*A->ib()];
-
 				int info = core_dtsqrt( A->mb(ti,tk), A->nb(ti,tk), A->ib(),
 						A->ttop(tk,tk), A->mb(tk,tk),
 						A->ttop(ti,tk), A->mb(ti,tk),
@@ -103,15 +99,12 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 				#pragma omp critical
 				cout << omp_get_thread_num() << " : " << "TSQRT(" << ti << "," << tk << "," << tk << ") : " << omp_get_wtime() - ttime << "\n";
 				#endif
-
-				delete[] Tau;
-				delete[] Work;
 			}
 
 			#pragma omp parallel for
 			for (int tj=tk+1; tj < NT; tj++)
 			{
-				double *Work = new double [A->ib()*A->nb(tk,tj)];
+				double *tWork = new double [A->ib()*A->nb(tk,tj)];
 
 				int info = core_dtsmqr( PlasmaLeft, PlasmaTrans,
 						A->mb(tk,tj), A->nb(tk,tj), A->mb(ti,tj), A->nb(ti,tj), A->nb(ti,tk), A->ib(),
@@ -119,7 +112,7 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 						A->ttop(ti,tj), A->mb(ti,tj),
 						A->ttop(ti,tk), A->mb(ti,tk),
 						T->ttop(ti,tk), T->mb(ti,tk),
-						Work,  A->ib());
+						tWork,  A->ib());
 				if (info != PlasmaSuccess)
 				{
 					cerr << "core_dtsmqr() failed\n";
@@ -131,11 +124,13 @@ void tileQR( TileMatrix *A, TileMatrix *T )
 				cout << omp_get_thread_num() << " : " << "SSRFB(" << ti << "," << tj << "," << tk << ") : " << omp_get_wtime() - ttime << "\n";
 				#endif
 
-				delete[] Work;
-
+				delete[] tWork;
 			} // j-LOOP END
 		} // i-LOOP END
 	} // k-LOOP END
+
+	delete [] Work;
+	delete [] Tau;
 	//////////////////////////////////////////////////////////////////////
 	// Right Looking tile QR END
 	//////////////////////////////////////////////////////////////////////
